9752539b1b2c3c7360d3c7cf5cdcb6f00cab54a5
Ivan <onel1n.van@yandex.ru>
2025-02-12 23:28:26 +0300

    added input and button

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>TodoApp</title>
 </head>
+
 <body>
-    
+    <div class="input-group mb-3" style="margin: 10% auto;  width: 40em;  height: 2em;">
+        <input type="text" class="form-control" placeholder="Add task" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2">+</button>
+    </div>
 </body>
+
 </html>

7c439cf2f2164c219d0bcde1b9213bdc65f19de6
Ivan <onel1n.van@yandex.ru>
2025-02-14 00:44:26 +0300

    input and button added

--- index.php
+++ index.php
@@ -3,11 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css">
     <title>TodoApp</title>
 </head>
 <body>
     <div class="wrapper">
+        <div class="field-input">
+            <input type="text" size=auto placeholder="Add task...">
+            <button><i class="bi bi-plus-lg" style="font-size: 20px;"></i></button>
+        </div>
         
     </div>
 

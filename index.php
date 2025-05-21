f3ffadf91893a27211bf77545bfd8c5beea9ddc3
VanLin <onel1n.van@yandex.ru>
2025-05-21 23:37:33 +0300

    carcass

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href = "styles.css">
+    <title>ToDoApp</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="logo">
+            <h1>ToDoApp</h1>
+            <h4>"Today we do this!"</h4>
+        </div>
+        <div class="field-input">
+            <input type="text">
+            <button>+</button>
+        </div>
+        <div class="field-tab">
+            <div class="tab-links">
+                <button class="tablinks">All</button>
+                <button class="tablinks">Active</button>
+                <button class="tablinks">Completed</button>
+            </div>
+            <div class="tab-content">
+                <div id="London" class="tabcontent">
+                <h3>London</h3>
+                <p>London is the capital city of England.</p>
+                </div>
+
+                <div id="Paris" class="tabcontent">
+                <h3>Paris</h3>
+                <p>Paris is the capital of France.</p>
+                </div>
+
+                <div id="Tokyo" class="tabcontent">
+                <h3>Tokyo</h3>
+                <p>Tokyo is the capital of Japan.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<footer>
+    <a href="https://github.com/VanLin444">My Github</a>
+</footer>
+</html>
